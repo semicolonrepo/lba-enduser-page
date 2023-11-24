8eed33150ed3112f9c266ee2c3a3a9bf6fc46ea4
Vieri Lusen <bluesdevil_vieri@yahoo.com>
2023-11-25 00:24:17 +0700

    Campaign Preview - change url iframe preview

--- app/Models/CustomerUser.php
+++ app/Models/CustomerUser.php
@@ -8,4 +8,16 @@ use Illuminate\Notifications\Notifiable;
 class CustomerUser extends Authenticatable
 {
     use Notifiable;
+
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'google_id',
+    ];
 }
