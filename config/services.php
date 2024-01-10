0677d209ee47454d07dc5ee0b5493f215f008f25
Vieri Lusen <bluesdevil_vieri@yahoo.com>
2024-01-10 12:18:34 +0700

    update google client redirect use env

--- config/services.php
+++ config/services.php
@@ -34,7 +34,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'https://fronttestonly.letsbuyasia.id/auth/google/callback',
+        'redirect' => env('GOOGLE_CLIENT_REDIRECT'),
     ],
 
 ];
