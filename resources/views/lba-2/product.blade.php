51fdd42e75a94d671dac86b057e11401be22e57c
Anggito Wicaksono <anggitov@gmail.com>
2024-03-21 16:30:45 +0700

    LBA107 - Feature - limit claim and send claim_qty

--- resources/views/lba-2/product.blade.php
+++ resources/views/lba-2/product.blade.php
@@ -24,115 +24,115 @@
   </div>
 </div>
 
-<!--====================  product content ====================-->
-<div style="margin-top:-100px" class="product-content-header-area space-pt--100 space-pb--25">
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <div class="product-content-header justify-content-center">
-          <div class="product-content-header__main-info">
-            <h3 class="title text-center fw-bold" style="font-size: 24px">
-              @if($product->normal_price == 0)
-                GRATIS
-              @elseif($product->normal_price != 0 && $product->subsidi_price == 0)
-                <p>{{ formatCurrency($product->normal_price) }}</p>
-              @else
-                <p>SEKARANG HANYA <span class="d-inline-block">{{ formatCurrency($product->normal_price -  $product->subsidi_price) }}</span></p>
+<form method="POST" id="form-get-voucher">
+  @csrf
+  <!--====================  product content ====================-->
+  <div style="margin-top:-100px" class="product-content-header-area space-pt--100 space-pb--25">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="product-content-header justify-content-center">
+            <div class="product-content-header__main-info">
+              <h3 class="title text-center fw-bold" style="font-size: 24px">
+                @if($product->normal_price == 0)
+                  GRATIS
+                @elseif($product->normal_price != 0 && $product->subsidi_price == 0)
+                  <p>{{ formatCurrency($product->normal_price) }}</p>
+                @else
+                  <p>SEKARANG HANYA <span class="d-inline-block">{{ formatCurrency($product->normal_price -  $product->subsidi_price) }}</span></p>
+                @endif
+              </h3>
+              <p class="text-center mt-4 mb-2">Jumlah Voucher</p>
+              <div class="d-flex justify-content-center">
+                <button type="button" id="decrement">-</button>
+                <input type="text" max="{{ $product->limit_claim }}" name="claim_qty" id="quantity" readonly>
+                <button type="button" id="increment">+</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- product content description -->
+  @if ($product->description)
+  <div class="product-content-description space-pb--25 pt-3">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-10">
+          <p class="section-title fw-bold m-0">Tentang Produk:</p>
+          <p class="section-title space-mb--25 lh-sm" style="font-size: 16px">
+            {!! $product->description !!}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  @endif
+
+  <div class="accordion accordion-flush">
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="term-condition">
+        <button class="accordion-button accordion-lba-2 collapsed fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#panel-term-condition" aria-expanded="false" aria-controls="panel-term-condition">
+          Syarat dan Ketentuan
+        </button>
+      </h2>
+      <div id="panel-term-condition" class="accordion-collapse collapse" aria-labelledby="term-condition">
+        <div class="accordion-body campaign-term-condition">
+          {!! $data->campaign_detail !!}
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+        <button class="accordion-button accordion-lba-2 collapsed fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+          Lokasi Merchant
+        </button>
+      </h2>
+      <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+        <div class="accordion-body">
+          <ul>
+          @forelse ($merchantCities as $merchantCity)
+            <li>{{ $merchantCity->name }}</li>
+          @empty
+            -
+          @endforelse
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="grand-total space-pb--15" style="border-radius: 0 0 12px 12px">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="total-shipping pb-0">
+            <h3 class="section-title fw-bold text-center mb-1">Pilih Lokasi Penukaran Voucher</h3>
+            <h6 class="text-center mb-3">Klik pada logo merchant</h6>
+            <div class="row" id="list-partner" style="row-gap: 16px">
+              @foreach ($retailer as $retailPartner)
+              <div class="col-6">
+                <label for="{{ $retailPartner->id }}" class="select-partner" data-partner-checked="{{ session('partner') == $retailPartner->id ? 'true' : 'false' }}">
+                  <img src="{{ env('BASE_URL_DASHBOARD').'/assets/provider/images/'.$retailPartner->photo }}" height="45px">
+                  <input type="radio" name="partner" class="d-none partner" value="{{ $retailPartner->id }}" id="{{ $retailPartner->id }}" {{ session('partner') == $retailPartner->id ? 'checked' : '' }}>
+                </label>
+              </div>
+              @endforeach
+              @if($internal->isNotEmpty())
+              <div class="col-6">
+                <label for="internal" class="select-partner" data-partner-checked="{{ session('partner') == 'internal' ? 'true' : 'false' }}">
+                  <p class="m-0 internal-partner">Merchant Partner Kami</p>
+                  <input type="radio" name="partner" class="d-none partner" value="internal" id="internal" {{ session('partner') == 'internal' ? 'checked' : '' }}>
+                </label>
+              </div>
               @endif
-            </h3>
-            <p class="text-center mt-4 mb-2">Jumlah Voucher</p>
-            <div class="d-flex justify-content-center">
-              <button id="decrement">-</button>
-              <input type="text" max="5" name="quantity" id="quantity" readonly>
-              <button id="increment">+</button>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  </div>
-</div>
 
-<!-- product content description -->
-@if ($product->description)
-<div class="product-content-description space-pb--25 pt-3">
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-10">
-        <p class="section-title fw-bold m-0">Tentang Produk:</p>
-        <p class="section-title space-mb--25 lh-sm" style="font-size: 16px">
-          {!! $product->description !!}
-        </p>
-      </div>
-    </div>
-  </div>
-</div>
-@endif
-
-<div class="accordion accordion-flush">
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="term-condition">
-      <button class="accordion-button accordion-lba-2 collapsed fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#panel-term-condition" aria-expanded="false" aria-controls="panel-term-condition">
-        Syarat dan Ketentuan
-      </button>
-    </h2>
-    <div id="panel-term-condition" class="accordion-collapse collapse" aria-labelledby="term-condition">
-      <div class="accordion-body campaign-term-condition">
-        {!! $data->campaign_detail !!}
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-      <button class="accordion-button accordion-lba-2 collapsed fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-        Lokasi Merchant
-      </button>
-    </h2>
-    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
-      <div class="accordion-body">
-        <ul>
-        @forelse ($merchantCities as $merchantCity)
-          <li>{{ $merchantCity->name }}</li>
-        @empty
-          -
-        @endforelse
-        </ul>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="grand-total space-pb--15" style="border-radius: 0 0 12px 12px">
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <div class="total-shipping pb-0">
-          <h3 class="section-title fw-bold text-center mb-1">Pilih Lokasi Penukaran Voucher</h3>
-          <h6 class="text-center mb-3">Klik pada logo merchant</h6>
-          <div class="row" id="list-partner" style="row-gap: 16px">
-            @foreach ($retailer as $retailPartner)
-            <div class="col-6">
-              <label for="{{ $retailPartner->id }}" class="select-partner" data-partner-checked="{{ session('partner') == $retailPartner->id ? 'true' : 'false' }}">
-                <img src="{{ env('BASE_URL_DASHBOARD').'/assets/provider/images/'.$retailPartner->photo }}" height="45px">
-                <input type="radio" name="partner" class="d-none partner" value="{{ $retailPartner->id }}" id="{{ $retailPartner->id }}" {{ session('partner') == $retailPartner->id ? 'checked' : '' }}>
-              </label>
-            </div>
-            @endforeach
-            @if($internal->isNotEmpty())
-            <div class="col-6">
-              <label for="internal" class="select-partner" data-partner-checked="{{ session('partner') == 'internal' ? 'true' : 'false' }}">
-                <p class="m-0 internal-partner">Merchant Partner Kami</p>
-                <input type="radio" name="partner" class="d-none partner" value="internal" id="internal" {{ session('partner') == 'internal' ? 'checked' : '' }}>
-              </label>
-            </div>
-            @endif
-          </div>
-        </div>
-      </div>
-
-      <div class="col-12">
-        <form method="POST" id="form-get-voucher">
-          @csrf
+        <div class="col-12">
           <div class="alert alert-danger m-0 mt-2 d-none" role="alert" id="alert"></div>
           <div class="total-shipping pb-2 pt-0">
             @if ($product->questionares_json != null)
@@ -263,12 +263,12 @@
               </div>
             @endif
           </div>
-        </form>
+        </div>
       </div>
     </div>
   </div>
-</div>
-<!--====================  End of product content  ====================-->
+  <!--====================  End of product content  ====================-->
+</form>
 
 <div class="category-slider-area space-pb--25 ">
   <div class="container">
